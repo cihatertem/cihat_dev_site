e6cf475edd400570d9e8e786bbfba628ec3c40c3
Cihat Ertem <cihatertem@gmail.com>
2022-07-26 20:10:11 +0300

    token end points

--- api/urls.py
+++ api/urls.py
@@ -10,7 +10,7 @@ app_name = 'api'
 
 urlpatterns = [
     path("blog/", include("blog.urls", namespace="blog")),
-    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('token', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify', TokenVerifyView.as_view(), name='token_verify'),
 ]
