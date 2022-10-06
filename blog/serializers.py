11d4a6c5fdfeb31e300c224827afe9da7e9bf31b
Cihat Ertem <cihatertem@gmail.com>
2022-10-06 12:33:52 +0300

    category create fixedv3!

--- blog/serializers.py
+++ blog/serializers.py
@@ -48,17 +48,13 @@ class PostSerializer(serializers.ModelSerializer):
     # comments = CommentSerializer(many=True, required=False)
 
     def create(self, validated_data):
-        tags = validated_data.pop('tags', None)
-        category = validated_data.pop('category', None)
+        tags = validated_data.pop('tags', [])
+        category = validated_data.pop('category', {})
         post = Post.objects.create(**validated_data)
 
-        if tags is not None:
-            post.tags.clear()
-            self._get_or_create_tags(tags, post)
-
-        if category is not None:
-            post.category.delete()
-            self._get_or_create_category(category, post)
+        self._get_or_create_tags(tags, post)
+        self._get_or_create_category(category, post)
+        
         return post
 
     def update(self, instance: Post, validated_data):
