2f0668a060c7a17852ba6411c35c75f9f130cd1c
Cihat Ertem <cihatertem@gmail.com>
2022-10-23 17:10:58 +0300

    foto image resizev6

--- base/utils.py
+++ base/utils.py
@@ -8,6 +8,7 @@ def work_directory_path(instance, filename):
 
 
 def photo_resizer(image: Image, size: int) -> BytesIO:
+    image = Image.open(image)
     output = BytesIO()
     if image.mode in ("RGBA", "P"):
         image = image.convert("RGB")
